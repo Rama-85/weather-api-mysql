3c5cfd5aa32d50fee1938c88090770e1b109db25
Rama-85 <ramadevimuttineni@gmail.com>
2023-07-31 16:54:16 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+type celsius float64
+type km float64
+type mm int
+
+type Airtemp struct {
+	maximum, minimum celsius
+}
+
+type Roadtemp struct {
+	maximum, minimum celsius
+}
+
+type Visibility struct {
+	maximum, minimum km
+}
+
+type Rain struct {
+	maximum, minimum mm
+}
+
+type WeatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Airtemp       `json:"Airtemp"`
+		AirPressure   float64 `json:"AirPressure"`
+		Humidity      float64 `json:"Humidity"`
+		WindDirection string  `json:"WindDirection"`
+		Roadtemp      `json:"Roadtemp"`
+		Visibility    `json:"Visibility"`
+		Windspeed     float64 `json:"Windspeed"`
+		Rain          `json:"Rain"`
+	} `json:"main"`
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from GoLang!\n"))
+}
+
+func query(city string) (WeatherData, error) {
+	//apiConfig, err := loadApiConfig(".apiConfig")
+	//if err != nil {
+	//	return WeatherData{}, err
+
+	resp, err := http.Get("http://api.open-meteo.com/v1/forecast?" + "&q=" + city)
+	if err != nil {
+		return WeatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d WeatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return WeatherData{}, err
+	}
+	return d, nil
+
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather/",
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := query(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(data)
+		})
+
+	http.ListenAndServe(":8080", nil)
+}
